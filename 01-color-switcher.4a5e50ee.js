0998886d828bdd63fe5f8ca292256ad18dc5a21d
pecherska <97174413+pecherska@users.noreply.github.com>
2022-08-19 11:56:00 +0000

    Deploying to gh-pages from @ pecherska/goit-js-hw-09@a8437f1768dd5b30a8eccec4d911b812de72e059 🚀

---
+++ 01-color-switcher.4a5e50ee.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body"),n=null;e.addEventListener("click",(function(){n=setInterval((function(){d.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(function(){clearInterval(n),e.disabled=!1,t.disabled=!0})),t.disabled=!0}();
+//# sourceMappingURL=01-color-switcher.4a5e50ee.js.map
